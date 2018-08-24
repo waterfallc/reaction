422c1a05f37dbbf8c3e474edc684f13ef65bb472
griggheo <grig@gheorghiu.net>
2018-08-24 12:14:46 -0700

    deploy multiple apps

--- .circleci/bin/ecs-deploy.sh
+++ .circleci/bin/ecs-deploy.sh
@@ -10,41 +10,47 @@ if [ -z "${AWS_REGION}" ]; then
         AWS_REGION=us-west-2
 fi
 
-APP_DIR_NAME=devops/aws/app-reaction
-MANIFEST_FILE="${APP_DIR_NAME}/manifest.yaml"
-if [ ! -f ${MANIFEST_FILE} ]; then
-    echo "Application manifest file not found!"
-    exit 1
-fi
+APP_DIR_NAME=devops/aws
+APPS=$(ls ${APP_DIR_NAME})
 
-ENV_NAME=$(/usr/local/bin/yq r $MANIFEST_FILE "environment.name")
+for APP in $APPS; do
 
-ENV_NAME_UPPERCASE=$(echo $ENV_NAME | awk '{print toupper($0)}')
-AWS_ACCESS_KEY_ID_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_SECRET_ACCESS_KEY
+	MANIFEST_FILE="${APP_DIR_NAME}/${APP}/manifest.yaml"
+	if [ ! -f ${MANIFEST_FILE} ]; then
+	    echo "Application manifest file not found!"
+	    exit 1
+	fi
 
-if [ "${!AWS_ACCESS_KEY_ID_VAR_NAME}" ]; then
-        AWS_ACCESS_KEY_ID=${!AWS_ACCESS_KEY_ID_VAR_NAME}
-fi
+	ENV_NAME=$(/usr/local/bin/yq r $MANIFEST_FILE "environment.name")
 
-if [ "${!AWS_SECRET_ACCESS_KEY_VAR_NAME}" ]; then
-        AWS_SECRET_ACCESS_KEY=${!AWS_SECRET_ACCESS_KEY_VAR_NAME}
-fi
+	ENV_NAME_UPPERCASE=$(echo $ENV_NAME | awk '{print toupper($0)}')
+	AWS_ACCESS_KEY_ID_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_ACCESS_KEY_ID
+	AWS_SECRET_ACCESS_KEY_VAR_NAME=CLOUDFORMATION_${ENV_NAME_UPPERCASE}_AWS_SECRET_ACCESS_KEY
 
-mkdir ~/.aws
-echo "[default]" > ~/.aws/credentials
-echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
-echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
+	if [ "${!AWS_ACCESS_KEY_ID_VAR_NAME}" ]; then
+		AWS_ACCESS_KEY_ID=${!AWS_ACCESS_KEY_ID_VAR_NAME}
+	fi
 
-echo "[default]" > ~/.aws/config
-echo "region = ${AWS_REGION}" >> ~/.aws/config
+	if [ "${!AWS_SECRET_ACCESS_KEY_VAR_NAME}" ]; then
+		AWS_SECRET_ACCESS_KEY=${!AWS_SECRET_ACCESS_KEY_VAR_NAME}
+	fi
 
-echo Running aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
-aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
+	mkdir ~/.aws
+	echo "[default]" > ~/.aws/credentials
+	echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
+	echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
 
-sudo mv propel /usr/local/bin/propel
-sudo chmod +x /usr/local/bin/propel
+	echo "[default]" > ~/.aws/config
+	echo "region = ${AWS_REGION}" >> ~/.aws/config
 
-cd ${APP_DIR_NAME}
-RELEASE_DESCRIPTION="CircleCI build URL: ${CIRCLE_BUILD_URL}"
-propel release create --deploy --descr "${RELEASE_DESCRIPTION}"
+	echo Running aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
+	aws s3 cp s3://${S3_PROPEL_ARTIFACTS_BUCKET}/propel-linux-amd64 ./propel
+
+	sudo mv propel /usr/local/bin/propel
+	sudo chmod +x /usr/local/bin/propel
+
+	cd ${APP_DIR_NAME}/${APP}
+	RELEASE_DESCRIPTION="CircleCI build URL: ${CIRCLE_BUILD_URL}"
+	propel release create --deploy --descr "${RELEASE_DESCRIPTION}"
+	cd -
+done
